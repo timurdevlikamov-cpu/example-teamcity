d648567cf5e230f9921ecf8f463e4fa355149c18
Timur <timur.devlikamov@gmail.com>
2026-08-08 23:44:41 +0300

    Add sayHunter method and test

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -15,4 +15,8 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+
+	public String sayHunter() {
+		return "The hunter is approaching!";
+	}
 }
--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -32,4 +32,9 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+
+	@Test
+    public void welcomerSaysHunterNew() {
+        assertThat(welcomer.sayHunter(), containsString("hunter"));
+    }
 }
